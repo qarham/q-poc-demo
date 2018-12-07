4d7fedc8612796db1c3d2227af7155b58c4bc2eb
qarham <qarham@juniper.net>
2018-12-06 18:18:40 -0600

    Fixed ansible install and added OS queens

--- poc01-iot/scripts/install_ubuntu_sw.sh
+++ poc01-iot/scripts/install_ubuntu_sw.sh
@@ -1,27 +1,34 @@
 #!/bin/bash -eux
 
 #login as root
+#sudo apt-get -f install
 
 sudo apt-get update
-sudo apt-get install \
-     python-minimal \
-     python-setuptools \
-     python-yaml \
-     python-paramiko \
-     python-jinja2 \
-     python-httplib2 \
-     git \
-     wget \
-     python-pip \
-     python-dev \
-     build-essential \
-     python-pip
-     apt-transport-https \
-     createrepo \
-     rpm2cpio  -y
+sudo apt-add-repository ppa:ansible/ansible-2.5
+sudo apt-get update
+sudo apt-get -y install ansible
 
-sudo wget https://launchpad.net/~ansible/+archive/ubuntu/ansible-2.4/+files/ansible_2.4.2.0-1ppa~zesty_all.deb
 
-sudo apt-get -f install
+#sudo apt-get update
+#sudo apt-get install \
+#     python-minimal \
+#     python-setuptools \
+#     python-yaml \
+#     python-paramiko \
+#     python-jinja2 \
+#     python-httplib2 \
+#     git \
+#     wget \
+#     python-pip \
+#     python-dev \
+#     build-essential \
+#     python-pip
+#     apt-transport-https \
+#     createrepo \
+#     rpm2cpio  -y
 
-sudo dpkg -i ansible_2.4.2.0-1ppa~zesty_all.deb
+#sudo wget https://launchpad.net/~ansible/+archive/ubuntu/ansible-2.4/+files/ansible_2.4.2.0-1ppa~zesty_all.deb
+
+#sudo apt-get -f install
+
+#sudo dpkg -i ansible_2.4.2.0-1ppa~zesty_all.deb
